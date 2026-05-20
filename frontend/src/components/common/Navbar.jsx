b166048e059c3953fd3cd2a6f08eeb2ddd5bc847
aniketbarai <aniketbarai677@gmail.com>
2026-05-20 20:57:15 +0530

    Navbar updated

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -3,21 +3,23 @@ import React from 'react'
 import { Truck, Gift, RefreshCw, Search, ChevronDown, Heart, ShoppingCart, CircleUserRound } from 'lucide-react'
 
 const Navbar = () => {
+
+    const features = ["SHOP","COLLECTION","NEW ARRIVAL","BESTSELLERS","ABOUT US"];
   return (
-     <div className='bg-amber-50 h-20 w-full flex items-center pr-5 pl-5 gap-10'>
-                        <div className='w-60 flex justify-center'>
+     <div className='bg-amber-50 h-16 w-full flex items-center justify-between gap-8 px-5'>
+
+                        <div className='flex justify-center'>
                             <div className="text-3xl font-bold text-black">CRAFTON</div>
                         </div>
     
-                        <div className='flex justify-center gap-8 pr-2 pl-2 text-sm'>
-                            <span className='flex '>SHOP<ChevronDown strokeWidth={1} /></span>
-                            <span>COLLECTION</span>
-                            <span>NEW ARRIVALS</span>
-                            <span>BESTSELLERS</span>
-                            <span>ABOUT US</span>
+                        <div className='flex text-sm justify-center items-center gap-8'>
+                            {features.map((e)=>(
+                                <button className='font-semibold cursor-pointer'>{e}</button>    
+                            ))}
                         </div>
     
-                        <div className="w-75 flex items-center border border-gray-300 rounded-full px-4 py-2">
+                        {/* search bar */}
+                        <div className="w-72 flex items-center border border-gray-300 rounded-full px-4 py-2">
                             <input
                                 type="text"
                                 placeholder='Search bags...'
@@ -25,12 +27,11 @@ const Navbar = () => {
                             />
                             <Search className='w-5 h-5 text-gray-500' />
                         </div>
-                        <div className='w-50 flex justify-center gap-4 '>
-                            <div className='font-extralight text-xs flex flex-col items-center'><Heart strokeWidth={1} />Wishlist</div>
-    
-                            <div className='font-extralight text-xs flex flex-col items-center'><ShoppingCart strokeWidth={1} />Wishlist</div>
-    
-                            <div className='font-extralight text-xs flex flex-col items-center'><CircleUserRound strokeWidth={1} />Wishlist</div>
+
+                        <div className=' flex justify-center gap-4'>
+                            <div className='font-semibold text-xs flex flex-col items-center'><Heart strokeWidth={2} />Wishlist</div>
+                            <div className='font-semibold text-xs flex flex-col items-center'><ShoppingCart strokeWidth={2} />Cart</div>
+                            <div className='font-semibold text-xs flex flex-col items-center'><CircleUserRound strokeWidth={2} />Wishlist</div>
                         </div>
     
                     </div>
