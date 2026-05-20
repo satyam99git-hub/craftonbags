4e4d9b7607e7a832610752d35da1c2b2263f4419
primecodestudio74-ui <your-email@github.com>
2026-05-20 22:33:41 +0530

    MongoDB Connection successfull !

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,1 +1,18 @@
 // Connects the backend application to the database.
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log("Database Error:", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,13 @@
-import "dotenv/config";
 import app from "./app.js";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Crafton Bags API running on http://localhost:${PORT}`);
+  console.log(`Crafton bag API running on http://localhost:${PORT}`);
 });
